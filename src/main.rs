31773efa3753b509b4bbc1a7f4f9a605c8157350
MarvinTheMoodLifter <marvin.themoodlifter@proton.me>
2024-04-11 13:14:59 +0200

    cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("action: {:?}, item: {:?}", action, item);
+}
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
 }
